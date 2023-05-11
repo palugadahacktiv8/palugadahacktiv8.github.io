69a0a75748e2a5ad1dfddae45e84a89005091b9d
fransalwan <fransalwan55@gmail.com>
2023-05-11 14:33:44 +0700

    datanya guys

---
+++ index.js
@@ -0,0 +1,38 @@
+let data = [
+  {
+    id: 1,
+    name: "Lampu Meja",
+    harga: 2000,
+    rating: [8, "barangnya keren"],
+  },
+  {
+    id: 2,
+    name: "Sofa",
+    harga: 45000,
+    rating: [8, "barangnya keren"],
+  },
+  {
+    id: 3,
+    name: "Meja",
+    harga: 25000,
+    rating: [8, "barangnya keren"],
+  },
+  {
+    id: 4,
+    name: "Kursi",
+    harga: 25000,
+    rating: [8, "barangnya keren"],
+  },
+  {
+    id: 5,
+    name: "Cermin",
+    harga: 30000,
+    rating: [8, "barangnya keren"],
+  },
+  {
+    id: 6,
+    name: "Lemari",
+    harga: 60000,
+    rating: [8, "barangnya keren"],
+  },
+];
